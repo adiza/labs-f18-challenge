8fc0192eab7e6bcb5f68be0ae41659d90c113dea
adiza <adiza.awwal@gmail.com>
2018-04-20 22:48:33 -0400

    removed commets added spacing

--- app.py
+++ app.py
@@ -5,19 +5,24 @@ app = Flask(__name__)
 
 @app.route('/pokemon/<username>')
 def show_user_profile(username):
-    # show the user profile for that user
-        # show the post with the given id, the id is an integer
+
     url = 'http://pokeapi.co/api/v2/pokemon/' + username +'/'
+    
     r = requests.get(url)
+
     u = r.json()['id']
+
     return '<h1> The pokemon with name {0} has id {1}</h1>'.format(username, u)
 
 @app.route('/pokemon/<int:post_id>')
 def show_post(post_id):
-    # show the post with the given id, the id is an integer
+
     url = 'http://pokeapi.co/api/v2/pokemon/' + str(post_id) +'/'
+
     r = requests.get(url)
+
     u = r.json()['name']
+
     return '<h1>The pokemon with id {0} is {1}</h1>'.format(post_id, u)
 
 
